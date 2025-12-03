6bcab4cf22be6d3ca89253371a084fc8c5f72e79
Polash Mahmud <polashmahmud@gmail.com>
2025-12-03 18:12:54 +0600

    refactor: enhance AppSidebarHeader with avatar and dropdown menu integration

--- resources/js/components/vitti/layouts/flat/AppSidebarHeader.vue
+++ resources/js/components/vitti/layouts/flat/AppSidebarHeader.vue
@@ -2,6 +2,8 @@
 import Breadcrumbs from '@/components/Breadcrumbs.vue';
 import { SidebarTrigger } from '@/components/ui/sidebar';
 import type { BreadcrumbItemType } from '@/types';
+import { Avatar, AvatarFallback, AvatarImage, } from "@/components/ui/avatar"
+import { DropdownMenu, DropdownMenuContent, DropdownMenuGroup, DropdownMenuItem, DropdownMenuLabel, DropdownMenuSeparator, DropdownMenuTrigger, } from "@/components/ui/dropdown-menu"
 
 withDefaults(
     defineProps<{
@@ -16,11 +18,31 @@ withDefaults(
 <template>
     <header
         class="flex h-16 shrink-0 items-center gap-2 border-b border-sidebar-border/70 px-6 transition-[width,height] ease-linear group-has-data-[collapsible=icon]/sidebar-wrapper:h-12 md:px-4">
-        <div class="flex items-center gap-2">
-            <SidebarTrigger class="-ml-1" />
-            <template v-if="breadcrumbs && breadcrumbs.length > 0">
-                <Breadcrumbs :breadcrumbs="breadcrumbs" />
-            </template>
+        <div class="flex items-center justify-between w-full gap-2">
+            <div class="flex items-center gap-2">
+                <SidebarTrigger class="-ml-1" />
+                <template v-if="breadcrumbs && breadcrumbs.length > 0">
+                    <Breadcrumbs :breadcrumbs="breadcrumbs" />
+                </template>
+            </div>
+            <div>
+                <DropdownMenu>
+                    <DropdownMenuTrigger>
+                        <Avatar>
+                            <AvatarImage src="https://github.com/shadcn.png" alt="@shadcn" />
+                            <AvatarFallback>CN</AvatarFallback>
+                        </Avatar>
+                    </DropdownMenuTrigger>
+                    <DropdownMenuContent align="end">
+                        <DropdownMenuLabel>My Account</DropdownMenuLabel>
+                        <DropdownMenuSeparator />
+                        <DropdownMenuItem>Profile</DropdownMenuItem>
+                        <DropdownMenuItem>Billing</DropdownMenuItem>
+                        <DropdownMenuItem>Team</DropdownMenuItem>
+                        <DropdownMenuItem>Subscription</DropdownMenuItem>
+                    </DropdownMenuContent>
+                </DropdownMenu>
+            </div>
         </div>
     </header>
 </template>
